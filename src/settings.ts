c1765068e3bb60078910ccd50754b7c2a3952f97
proxima812 <mrzuc2015@yandex.ru>
2024-05-06 21:39:43 +0500

    first commit

---
+++ src/settings.ts
@@ -0,0 +1,24 @@
+export const config = {
+	site: {
+		base: {
+			url: "https://nadaily.vercel.app/",
+		},
+		header: {
+			head: "🔷 Ежедневные размышления Анонимных Наркоманов",
+		},
+		OG: {
+			site_name: "12 steps",
+			author: "samgold",
+			title: "Daily 12 Steps",
+			description: "Daily 12 Steps diaries",
+			locale: "ru",
+			defaultImage: "default-ogImage.png",
+		},
+	},
+	verifications: {
+		yandex: "",
+		google: "",
+		bing: "",
+		// ...
+	},
+}
